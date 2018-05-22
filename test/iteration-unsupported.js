b8e45698db22bcb77b25748e733b5b8848f3cee1
isaacs <i@izs.me>
2018-05-22 12:00:30 -0700

    add some missing tests

---
+++ test/iteration-unsupported.js
@@ -0,0 +1,17 @@
+'use strict'
+const t = require('tap')
+process.env._MP_NO_ITERATOR_SYMBOLS_ = '1'
+const MP = require('../index.js')
+const mp = new MP
+mp.write('foo')
+setTimeout(() => mp.end())
+t.throws(() => {
+  for (let x of mp) {
+    t.fail('should not be iterable')
+  }
+})
+t.rejects(async () => {
+  for await (let x of mp) {
+    t.fail('should not be async iterable')
+  }
+})
---
+++ test/iteration.js
@@ -0,0 +1,134 @@
+'use strict'
+const t = require('tap')
+const MP = require('../index.js')
+
+
+t.test('sync iteration', t => {
+  const cases = {
+    'buffer': [ null, [
+      Buffer.from('ab'),
+      Buffer.from('cd'),
+      Buffer.from('e')
+    ]],
+    'string': [ { encoding: 'utf8' }, ['ab', 'cd', 'e']],
+    'object': [ { objectMode: true }, ['a', 'b', 'c', 'd', 'e']]
+  }
+  const runTest = (c, opt, expect) => {
+    t.test(c, t => {
+      const result = []
+      const mp = new MP(opt)
+      mp.write('a')
+      mp.write('b')
+      for (let letter of mp) {
+        result.push(letter)
+      }
+      mp.write('c')
+      mp.write('d')
+      result.push.call(result, ...mp)
+      mp.write('e')
+      mp.end()
+      for (let letter of mp) {
+        result.push(letter) // e
+      }
+      for (let letter of mp) {
+        result.push(letter) // nothing
+      }
+      t.match(result, expect)
+      t.end()
+    })
+  }
+
+  for (let c in cases) {
+    runTest(c, cases[c][0], cases[c][1])
+  }
+
+  t.end()
+})
+
+t.test('async iteration', t => {
+  const expect = [
+    'start\n',
+    'foo\n',
+    'foo\n',
+    'foo\n',
+    'foo\n',
+    'foo\n',
+    'bar\n'
+  ]
+
+  t.test('end immediate', async t => {
+    const mp = new MP({ encoding: 'utf8' })
+    let i = 5
+
+    mp.write('start\n')
+    const inter = setInterval(() => {
+      if (i --> 0)
+        mp.write(Buffer.from('foo\n', 'utf8'))
+      else {
+        mp.end('bar\n')
+        clearInterval(inter)
+      }
+    })
+
+    const result = []
+    for await (let x of mp)
+      result.push(x)
+
+    t.same(result, expect)
+  })
+
+  t.test('end later', async t => {
+    const mp = new MP({ encoding: 'utf8' })
+    let i = 5
+
+    mp.write('start\n')
+    const inter = setInterval(() => {
+      if (i --> 0)
+        mp.write(Buffer.from('foo\n', 'utf8'))
+      else {
+        mp.write('bar\n')
+        setTimeout(() => mp.end())
+        clearInterval(inter)
+      }
+    })
+
+    const result = []
+    for await (let x of mp)
+      result.push(x)
+
+    t.same(result, expect)
+  })
+
+  t.test('throw error', async t => {
+    const mp = new MP()
+    const poop = new Error('poop')
+    setTimeout(() => {
+      mp.read = () => { throw poop }
+      mp.end('this is fine')
+    })
+    const result = []
+    const run = async () => {
+      for await (let x of mp) {
+        result.push(x)
+      }
+    }
+
+    await t.rejects(run, poop)
+  })
+
+  t.test('emit error', async t => {
+    const mp = new MP()
+    const poop = new Error('poop')
+    setTimeout(() => mp.emit('error', poop))
+    const result = []
+    const run = async () => {
+      for await (let x of mp) {
+        result.push(x)
+      }
+    }
+
+    await t.rejects(run, poop)
+  })
+
+  t.end()
+})
